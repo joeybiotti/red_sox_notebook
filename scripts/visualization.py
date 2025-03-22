bd52c45c4781a9e8fb21fc79f34bfa81b1390a6a
Joey Biotti <joeybiotti@Joeys-MacBook-Pro.local>
2025-03-21 22:08:19 -0400

    refactoed visualzations.py, added viz cell to notebook

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
-red_sox_data = pd.read_csv('data/cleaned/Boston_Red_Sox_Roster_Data_cleaned.csv')
+script_dir =os.path.dirname(os.path.abspath(__file__))
+
+csv_path = os.path.join(script_dir, '../data/cleaned/Boston_Red_Sox_Roster_Data_cleaned.csv')
+
+red_sox_data = pd.read_csv(csv_path)
 
 # Plotting the age distribution of Red Sox players
 red_sox_data['Age'].hist(bins=10, color='red', edgecolor='black')
@@ -10,13 +15,15 @@ plt.title('Age Distribution of Red Sox Players')
 plt.xlabel('Age')
 plt.ylabel('Number of Players')
 
-plt.savefig('./output/age_distribution.png')
+output_dir = os.path.join(script_dir, '../output')
+os.makedirs(output_dir, exist_ok=True)
+plt.savefig(os.path.join(output_dir, 'age_distribution.png'))
 
 # Plotting the age spread of Red Sox players
 sns.boxplot(y='Age', color='red', data=red_sox_data)
 plt.title('Age Spread of Red Sox Players')
 plt.ylabel('Age')
 
-plt.savefig('./output/age_spread.png')
+plt.savefig(os.path.join(output_dir, 'age_spread.png'))
 
 print('Visualization complete!')
